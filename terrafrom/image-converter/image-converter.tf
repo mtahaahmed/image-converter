b0d8213ac17234309d5568eab0521d35ec9d2b0e
M Taha Ahmed <taha.ahmed@deliveryhero.com>
2024-01-14 15:07:40 +0100

    adding app dependency

--- terrafrom/image-converter/image-converter.tf
+++ terrafrom/image-converter/image-converter.tf
@@ -3,4 +3,6 @@ resource "helm_release" "cert_manager" {
   chart      = "../../chart/image-converter"
   version    = "v1.13.3"
   timeout    = 900
+
+  depends_on = [google_pubsub_topic.example, google_service_account_key.example_key, google_sql_database_instance.main]
 }
