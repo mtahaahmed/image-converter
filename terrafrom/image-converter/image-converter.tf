5207466b7238ddd99636c10f44263a737ca03803
M Taha Ahmed <taha.ahmed@deliveryhero.com>
2024-01-14 20:01:11 +0100

    chart timeout too long

--- terrafrom/image-converter/image-converter.tf
+++ terrafrom/image-converter/image-converter.tf
@@ -2,7 +2,7 @@ resource "helm_release" "cert_manager" {
   name       = "image-converter"
   chart      = "../../chart/image-converter"
   version    = "v1.13.3"
-  timeout    = 900
+  timeout    = 200
 
   depends_on = [google_pubsub_topic.example, google_service_account_key.example_key, google_sql_database_instance.main]
 }
